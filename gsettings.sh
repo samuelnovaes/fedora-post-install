8fbc9a8a5ccd57a41778d3423cc1ea2afca3b3d6
Samuel Novaes <samuel.novaes.96@gmail.com>
2026-03-13 01:41:31 -0300

    Add sleep intervals to gsettings.sh for improved execution timing

--- gsettings.sh
+++ gsettings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 5
+
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/.local/share/backgrounds/wallpaper.jpg"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/.local/share/backgrounds/wallpaper.jpg"
 gsettings set org.gnome.desktop.interface enable-hot-corners false
@@ -11,10 +13,16 @@ gsettings set org.gnome.desktop.interface cursor-theme breeze_cursors
 gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'code.desktop', 'org.gnome.Ptyxis.desktop', 'com.mongodb.Compass.desktop', 'com.spotify.Client.desktop', 'com.discordapp.Discord.desktop', 'io.podman_desktop.PodmanDesktop.desktop', 'org.gnome.Nautilus.desktop', 'com.getpostman.Postman.desktop']"
 gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com', 'dash-to-dock@micxgx.gmail.com', 'blur-my-shell@aunetx']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'br')]"
+
+sleep 5
+
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DYNAMIC
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup true
 gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
+
+sleep 5
+
 gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur false
 
 systemctl --user disable gsettings.service
