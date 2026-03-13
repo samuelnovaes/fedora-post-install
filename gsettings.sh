3ee00055ec3846c23ec0b178ec33a4dfb3e2c3c6
Samuel Novaes <samuel.novaes.96@gmail.com>
2026-03-13 01:52:02 -0300

    Remove redundant sleep commands from gsettings.sh for streamlined execution

--- gsettings.sh
+++ gsettings.sh
@@ -13,16 +13,10 @@ gsettings set org.gnome.desktop.interface cursor-theme breeze_cursors
 gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'code.desktop', 'org.gnome.Ptyxis.desktop', 'com.mongodb.Compass.desktop', 'com.spotify.Client.desktop', 'com.discordapp.Discord.desktop', 'io.podman_desktop.PodmanDesktop.desktop', 'org.gnome.Nautilus.desktop', 'com.getpostman.Postman.desktop']"
 gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com', 'dash-to-dock@micxgx.gmail.com', 'blur-my-shell@aunetx']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'br')]"
-
-sleep 5
-
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DYNAMIC
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 gsettings set org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup true
 gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
-
-sleep 5
-
 gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur false
 
 systemctl --user disable gsettings.service
